63be88e898a4bc24ac7ebf418e813688ce787012
Melanion <megakeen@gmail.com>
2014-05-08 22:51:25 -0400

    Starting from scratch, some bootstrap stuff

--- vendingmachine/admin.py
+++ vendingmachine/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from vendingmachine.models import Caliber
+from vendingmachine.models import Bullet
+from vendingmachine.models import Purpose
+from vendingmachine.models import GoodFor
 
 # Register your models here.
+admin.site.register(Caliber)
+admin.site.register(Bullet)
+admin.site.register(Purpose)
+admin.site.register(GoodFor)
